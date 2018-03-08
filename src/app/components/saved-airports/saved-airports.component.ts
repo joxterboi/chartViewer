928073965eafed6687c14345bb5c1e084b9c4320
joxterboi <joa123kim456@gmail.com>
2018-03-08 15:33:19 +0100

    Added first components and full electron support

---
+++ src/app/components/saved-airports/saved-airports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-airports',
+  templateUrl: './saved-airports.component.html',
+  styleUrls: ['./saved-airports.component.css']
+})
+export class SavedAirportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/saved-plates/saved-plates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-plates',
+  templateUrl: './saved-plates.component.html',
+  styleUrls: ['./saved-plates.component.css']
+})
+export class SavedPlatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
